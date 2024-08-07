5812c2e74d76c9b53a2d5f1214c7b99fbf332636
KnyChe0705 <chohee200820@gmail.com>
2024-08-07 20:40:38 +0900

    2024.08.07 1st upload

---
+++ src/main/java/com/webBoard/WebBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.webBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBoardProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webBoard/WebBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
